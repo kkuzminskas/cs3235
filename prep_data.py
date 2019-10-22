fea7ffd772cb024b4872bab8851d12ac88456184
kkuzminskas <kendallkuzminskas2020@u.northwestern.edu>
2019-10-22 19:12:09 +0800

    Pre-processing eyetribe data
    Process the data (based on the eyetracker_output.txt file) and get it ready to be used in ML algorithms. It takes the data and puts it into a pandas data frame.  It is important to note that ML aglorithms can only take numerical values. Thus, the columns 'Fix', 'State', and 'Timestamp' will not be used in ML but are included here for consistency. These columns don't seem that helpful for our assignment anyway.

---
+++ prep_data.py
@@ -0,0 +1,54 @@
+import sklearn
+import json
+import pandas as pd
+import numpy as np
+
+files = open("eyetribe_output.txt", "r")
+data = files.read()
+d2 = json.loads(data)
+files.close()
+
+output_list = d2['all']
+final_output = {}
+
+## Preprocessing Data
+keys = list(output_list[0]['values']['frame'].keys())
+
+
+def empty_list(prev_key, keys, final_output, val):
+    prev_key_init = prev_key
+    for k in keys:
+        prev_key = prev_key_init
+        vals = val[k]
+        if type(vals) == type({}):
+            new_keys = vals.keys()
+            prev_key += k + '_'
+            empty_list(prev_key, new_keys, final_output, vals)
+        else: 
+            final_output[prev_key + k] = list()
+
+empty_list('', keys, final_output, output_list[0]['values']['frame'])
+
+def transform_vals(prev_key, keys, final_output, val):
+    prev_key_init = prev_key
+    for k in keys:
+        prev_key = prev_key_init
+        vals = val[k]
+        if type(vals) == type({}):
+            new_keys = vals.keys()
+            prev_key += k + '_'
+            transform_vals(prev_key, new_keys, final_output, vals)
+        else: 
+            final_output[prev_key + k].append(vals)
+
+for val in output_list:
+    if 'values' in val.keys():
+        transform_vals('', keys, final_output, val['values']['frame'])
+
+pd_data = pd.DataFrame.from_dict(final_output)
+print(pd_data.head())
+
+print(pd_data.columns.values.tolist())
+print(pd_data.shape)
+
+np_data = pd_data.to_numpy()

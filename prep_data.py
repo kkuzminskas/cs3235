b36ad48d84a572c174055b58878778e940522ca5
kkuzminskas <kendallkuzminskas2020@u.northwestern.edu>
2019-10-29 18:28:39 +0800

    Update prep_data.py

--- prep_data.py
+++ prep_data.py
@@ -1,20 +1,21 @@
-import sklearn
-import json
 import pandas as pd
 import numpy as np
-
-files = open("eyetribe_output.txt", "r")
-data = files.read()
-d2 = json.loads(data)
-files.close()
-
-output_list = d2['all']
-final_output = {}
-
-## Preprocessing Data
-keys = list(output_list[0]['values']['frame'].keys())
+import json
+import os
 
 
+def transform_file(file_name="eyetribe_output_copy.txt"):
+    file = open(file_name, "r+")
+    content = ""
+    for line in file:
+        content += line[:-1] + ", \n"
+    content = content[:-3] + "\n]}"
+    file.seek(0,0)
+    file.write('{"all": [\n' + content)
+    file.close()
+
+
+# Get data
 def empty_list(prev_key, keys, final_output, val):
     prev_key_init = prev_key
     for k in keys:
@@ -26,8 +27,6 @@ def empty_list(prev_key, keys, final_output, val):
             empty_list(prev_key, new_keys, final_output, vals)
         else: 
             final_output[prev_key + k] = list()
-
-empty_list('', keys, final_output, output_list[0]['values']['frame'])
 
 def transform_vals(prev_key, keys, final_output, val):
     prev_key_init = prev_key
@@ -41,21 +40,36 @@ def transform_vals(prev_key, keys, final_output, val):
         else: 
             final_output[prev_key + k].append(vals)
 
-for val in output_list:
-    if 'values' in val.keys():
-        transform_vals('', keys, final_output, val['values']['frame'])
 
-pd_data = pd.DataFrame.from_dict(final_output)
-print(pd_data.head())
+def clean_data(file_name="eyetribe_output_test.txt"):
+    files = open(file_name, "r")
+    data = files.read()
+    d2 = json.loads(data)
+    files.close()
 
-print(pd_data.columns.values.tolist())
-print(pd_data.shape)
+    output_list = d2['all']
+    final_output = {}
 
-np_data = pd_data.to_numpy()
+    ## Preprocessing Data
+    keys = list(output_list[0]['values']['frame'].keys())
 
-features = np.array(pd_data.columns.values.tolist())
-remove_feat = ["fix", "time", "timestamp", "state"]
-features = list(filter(lambda f: f not in remove_feat, features))
-print(features)
-# cleaned data with only float valued features
-X = pd_data[features]
+    empty_list('', keys, final_output, output_list[0]['values']['frame'])
+
+    
+    for val in output_list:
+        if 'values' in val.keys():
+            transform_vals('', keys, final_output, val['values']['frame'])
+
+    pd_data = pd.DataFrame.from_dict(final_output)
+    features = ["time", "avg_x", "avg_y"]
+    return pd_data[features]
+
+
+
+
+if __name__ == '__main__':
+    # get data
+    # data = clean_data("eyetribe_output_copy.txt")
+    transform_file()
+    data = clean_data("eyetribe_output_copy.txt")
+    
